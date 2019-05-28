f99ed40e7bfa5e0f2ab12ab7fb5f20cb448efb03
fadlytr <36650539+fadlytr@users.noreply.github.com>
2019-05-28 13:15:27 +0700

    add detect - and k for convert

--- DataCheck/src/main/java/com/mycompany/datacheck/Arrange10.java
+++ DataCheck/src/main/java/com/mycompany/datacheck/Arrange10.java
@@ -21,162 +21,251 @@ import jxl.write.WriteException;
  * @author AtikaP and edited by FadlyTr
  */
 public class Arrange10 {
+
     private static final String EXCEL_FILE_LOCATION = "E:\\Kuliah\\Proyek_2\\DATAFULL.xls";
-         private static final String EXCEL_WRITE_LOCATION = "E:\\Kuliah\\Proyek_2\\TEMP.xls";
+    private static final String EXCEL_WRITE_LOCATION = "E:\\Kuliah\\Proyek_2\\TEMP.xls";
 
     public static void main(String[] args) throws WriteException {
-        int row=0, jumpMatch = 19, rowWrite = 0, colWrite, rowT1 = 6, rowT2 = 13, i, j, k, TeamT1=4, TeamT2=11, winner=2;
+        int row = 0, jumpMatch = 19, rowWrite = 0, colWrite, rowT1 = 6, rowT2 = 13, i, j, k, TeamT1 = 4, TeamT2 = 11, winner = 2;
         Workbook workbook = null;
         try {
 
             workbook = Workbook.getWorkbook(new File(EXCEL_FILE_LOCATION));
             Sheet sheet = workbook.getSheet(8);
             Cell cell1, T1, T2, Winner;
-            
-            
+
             WritableWorkbook myFirstWbook = null;
             myFirstWbook = Workbook.createWorkbook(new File(EXCEL_WRITE_LOCATION));
             // create an Excel sheet
             WritableSheet excelSheet = myFirstWbook.createSheet("2017", 0);
-            Label label ;
-            label = new Label(0, rowWrite, "name0");
-            excelSheet.addCell(label);
-            label = new Label(1, rowWrite, "name1");
-            excelSheet.addCell(label);
-            label = new Label(2, rowWrite, "K0");
-            excelSheet.addCell(label);
-            label = new Label(3, rowWrite, "K1");
-            excelSheet.addCell(label);
-            label = new Label(4, rowWrite, "D0");
-            excelSheet.addCell(label);
-            label = new Label(5, rowWrite, "D1");
-            excelSheet.addCell(label);
-            label = new Label(6, rowWrite, "A0");
-            excelSheet.addCell(label);
-            label = new Label(7, rowWrite, "A1");
-            excelSheet.addCell(label);
-            label = new Label(8, rowWrite, "NET0");
-            excelSheet.addCell(label);
-            label = new Label(9, rowWrite, "NET1");
-            excelSheet.addCell(label);
-            label = new Label(10, rowWrite, "LH0");
-            excelSheet.addCell(label);
-            label = new Label(11, rowWrite, "LH1");
-            excelSheet.addCell(label);
-            label = new Label(12, rowWrite, "DN0");
-            excelSheet.addCell(label);
-            label = new Label(13, rowWrite, "DN1");
-            excelSheet.addCell(label);
-            label = new Label(14, rowWrite, "GPM0");
-            excelSheet.addCell(label);
-            label = new Label(15, rowWrite, "GPM1");
-            excelSheet.addCell(label);
-            label = new Label(16, rowWrite, "XPM0");
-            excelSheet.addCell(label);
-            label = new Label(17, rowWrite, "XPM1");
-            excelSheet.addCell(label);
-            label = new Label(18, rowWrite, "DMG0");
-            excelSheet.addCell(label);
-            label = new Label(19, rowWrite, "DMG1");
-            excelSheet.addCell(label);
-            label = new Label(20, rowWrite, "HEAL0");
-            excelSheet.addCell(label);
-            label = new Label(21, rowWrite, "HEAL1");
-            excelSheet.addCell(label);
-            label = new Label(22, rowWrite, "BLD0");
-            excelSheet.addCell(label);
-            label = new Label(23, rowWrite, "BLD1");
-            excelSheet.addCell(label);
-            label = new Label(24, rowWrite, "WIN0");
-            excelSheet.addCell(label);
-            label = new Label(25, rowWrite, "WIN1");
-            excelSheet.addCell(label);
+            Label label;
+            for (i = 0; i < 10; i++) {
+                label = new Label(i, rowWrite, "Hero" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 10; i < 20; i++) {
+                label = new Label(i, rowWrite, "Nam" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 20; i < 30; i++) {
+                label = new Label(i, rowWrite, "K" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 30; i < 40; i++) {
+                label = new Label(i, rowWrite, "D" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 40; i < 50; i++) {
+                label = new Label(i, rowWrite, "A" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 50; i < 60; i++) {
+                label = new Label(i, rowWrite, "NET" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 60; i < 70; i++) {
+                label = new Label(i, rowWrite, "LH" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 70; i < 80; i++) {
+                label = new Label(i, rowWrite, "DN" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 80; i < 90; i++) {
+                label = new Label(i, rowWrite, "GPM" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 90; i < 100; i++) {
+                label = new Label(i, rowWrite, "XPM" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 100; i < 110; i++) {
+                label = new Label(i, rowWrite, "DMG" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 110; i < 120; i++) {
+                label = new Label(i, rowWrite, "HEAL" + i % 10);
+                excelSheet.addCell(label);
+            }
+            for (i = 120; i < 130; i++) {
+                label = new Label(i, rowWrite, "BLD" + i % 10);
+                excelSheet.addCell(label);
+            }
+
             rowWrite++;
-            for(row = 0; row< 8778; row++){
-                for(i = 0; i<5; i++){
-                    for(j = 0; j<5; j++){
-                        colWrite = 0;
-                        for(k = 0; k<12; k++){
-                            cell1 = sheet.getCell(k+1, row+rowT1+i);
-                            label = new Label(colWrite, rowWrite, cell1.getContents());
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            cell1 = sheet.getCell(k+1, row+rowT2+j);
-                            label = new Label(colWrite, rowWrite, cell1.getContents());
-                            excelSheet.addCell(label);
-                            colWrite++;
-                        }
-                        //Check winner
-                        T1 = sheet.getCell(0, row+TeamT1);
-                        T2 = sheet.getCell(0, row+TeamT2);
-                        Winner = sheet.getCell(0, row+winner);
-                        if(Winner.getContents().substring(0, Winner.getContents().length()-9).equals(T1.getContents())){
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
+            String str = new String();
+            for (row = 0; row < 8778; row++) {
+                for (j = 0; j < 5; j++) {
+                    colWrite = 0;
+                    for (k = 0; k < 13; k++) {
+                        cell1 = sheet.getCell(k, row + rowT1 + j);
+                        str = cell1.getContents();
+                        if (str.equals("-")) {
+                            label = new Label(colWrite, rowWrite, "0");
+                        } else if (str.endsWith("k")) {
+                            label = new Label(colWrite, rowWrite, str.);
                         } else {
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"0");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
-                            label = new Label(colWrite, rowWrite,"1");
-                            excelSheet.addCell(label);
-                            colWrite++;
+                            label = new Label(colWrite, rowWrite, str);
                         }
-                        rowWrite++;
+                        excelSheet.addCell(label);
+                        colWrite++;
                     }
+                    //Check winner
+                    T1 = sheet.getCell(0, row + TeamT1);
+                    T2 = sheet.getCell(0, row + TeamT2);
+                    Winner = sheet.getCell(0, row + winner);
+                    if (Winner.getContents().substring(0, Winner.getContents().length() - 9).equals(T1.getContents())) {
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                    } else {
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                    }
+                    rowWrite++;
                 }
-                row+= jumpMatch;
+
+                for (j = 0; j < 5; j++) {
+                    colWrite = 0;
+                    for (k = 0; k < 13; k++) {
+                        cell1 = sheet.getCell(k, row + rowT2 + j);
+                        str = cell1.getContents();
+                        if (str.equals("-")) {
+                            label = new Label(colWrite, rowWrite, "0");
+                        } else if (str.endsWith("k")) {
+                            label = new Label(colWrite, rowWrite, str.);
+                        } else {
+                            label = new Label(colWrite, rowWrite, str);
+                        }
+                        excelSheet.addCell(label);
+                        colWrite++;
+                    }
+                    //Check winner
+                    T1 = sheet.getCell(0, row + TeamT1);
+                    T2 = sheet.getCell(0, row + TeamT2);
+                    Winner = sheet.getCell(0, row + winner);
+                    if (Winner.getContents().substring(0, Winner.getContents().length() - 9).equals(T1.getContents())) {
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                    } else {
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "0");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                        label = new Label(colWrite, rowWrite, "1");
+                        excelSheet.addCell(label);
+                        colWrite++;
+                    }
+                    rowWrite++;
+                }
+
+                row += jumpMatch;
             }
             myFirstWbook.write();
             if (myFirstWbook != null) {
@@ -200,5 +289,5 @@ public class Arrange10 {
             }
         }
     }
-   
+
 }
